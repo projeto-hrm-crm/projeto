86800b4c3e5b01237076d5aee448e3d46d1e85d5
Pedro Henrique Guimarães <pedro.guimaraes@alboompro.com.br>
2018-03-27 01:25:08 -0300

    Inserção do sufee admin template e configuração básica para o banco de dados

--- application/database.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$database_config = [
-	'host' 		=> '',
-	'user'		=> '',
-	'password' 	=> '',
-	'database'	=> '',
-];
---
+++ db_environment.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pedroguimaraes
+ * Date: 3/27/18
+ * Time: 1:18 AM
+ */
+
+$database_config = [
+    'host' 		=> 'localhost',
+    'user'		=> 'root',
+    'password' 	=> '',
+    'database'	=> '',
+];
