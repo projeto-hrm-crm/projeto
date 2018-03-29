1f85aeb2f7488f71e080f365cfd8ffe38cfcf804
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-03-29 16:40:46 -0300

    Validação de pessoa concluída backend

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -53,15 +53,69 @@ $config = array(
 $config = array(
     'pessoa/salvar' => 
     
-    array(
-        'field' => 'nome',
-        'label' => 'Nome',
-        'rules' => 'required'
-    ),
+        array(
+            'field' => 'nome',
+            'label' => 'Nome',
+            'rules' => 'required'
+        ),
 
-    array(
-        'field' => 'email',
-        'label' => 'E-Mail',
-        'rules' => 'valid_email'
-    ),
+        array(
+            'field' => 'email',
+            'label' => 'E-Mail',
+            'rules' => 'valid_email|max_length[100]'
+        ),
+
+        array(
+            'field' => 'cep',
+            'label' => 'CEP',
+            'rules' => 'required|min_length[11]'
+        ),
+
+        array(
+            'field' => 'estado',
+            'label' => 'Estado',
+            'rules' => 'required'
+        ),
+
+        array(
+            'field' => 'cidade',
+            'label' => 'Cidade',
+            'rules' => 'required'
+        ),
+
+        array(
+            'field' => 'bairro',
+            'label' => 'Bairro',
+            'rules' => 'required'
+        ),        
+
+        array(
+            'field' => 'logradouro',
+            'label' => 'Logradouro',
+            'rules' => 'required'
+        ),
+
+        array(
+            'field' => 'endereco_numero',
+            'label' => 'Número',
+            'rules' => 'required'
+        ),
+
+        array(
+            'field' => 'documento_numero',
+            'label' => 'Documento',
+            'rules' => 'required'
+        ),
+
+        array(
+            'field' => 'tipo',
+            'label' => 'Tipo',
+            'rules' => 'required'
+        ),
+
+        array(
+            'field' => 'telefone_numero',
+            'label' => 'Telefone',
+            'rules' => 'required'
+        ),
 );
