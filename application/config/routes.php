e7a01d04d89b2f5c7dd8aa7d2a5cbb72c397c790
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-03-29 10:51:51 -0300

    Criada rota para atualização de pessoa

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['pessoa/salvar'] = 'pessoa/save';
+
+$route['pessoa/salvar']    = 'pessoa/save';
+$route['pessoa/atualizar'] = 'pessoa/update';
