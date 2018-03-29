0c83f93e022a40932039e86ee862c826566d621b
Níkolas Lencioni <nikolasagl@hotmail.com>
2018-03-29 11:24:38 -0300

    Update index.php

--- application/views/fornecedor/index.php
+++ application/views/fornecedor/index.php
@@ -7,14 +7,9 @@
   <body>
     View Fornecedores
 
-<<<<<<< HEAD
     <?php foreach ($fornecedores as $fornecedor): ?>
       <?php echo $fornecedor->nome ?><br>
     <?php endforeach ?>
-=======
-    <!-- <?php foreach $data['fornecedores'] as $fornecedor: ?>
-      <?php echo $fornecedor->nome ?>
-    <?php endforeach ?> -->
->>>>>>> 66eb316325f9ad4dea1d60901dd0ed744888ab18
+
   </body>
 </html>
