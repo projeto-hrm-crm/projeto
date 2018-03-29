5d58262a1ac31f4ba702bd2aa21bdcb14794317f
Pedro Henrique <pedroguimaraes.contato@gmail.com>
2018-03-28 22:18:37 -0300

    Processo formulário de login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -7,10 +7,26 @@ class Login extends CI_Controller
      * Recebe os dados do formulário, trata e 
      * usa o model para busca do usuário
      * 
-     * @return boolean
+     * @return void
      */
     public function index()
     {
-        $data = $this->input->post('');
+        $data = $this->input->post();
+
+        if ($this->form_validation->run('login')) 
+            $this->Usuario_model->login($data);
+        else
+            $this->load->view('login/index.php');
+    }
+
+    /**
+     * @author Pedro Henrique Guimarães
+     * Destroi a sessão que mantem o usuário logado e desloga o mesmo
+     * 
+     * @return void
+     */
+    public function logout()
+    {
+        $this->session->unset_userdata('login');
     }
 }
---
+++ application/models/Usuario_model.php
@@ -0,0 +1,15 @@
+<?php 
+class Usuario_model extends CI_Model
+{
+    /**
+     * @author Pedro Henrique Guimarães
+     * Método responsável por efetuar o login
+     * 
+     * @param mixed
+     * @return boolean
+     */
+    public function login($data)
+    {
+
+    }
+}
