7c35765a075786971ce14f5a7308f9c29faeb4ad
Pssales <camilapsales27@gmail.com>
2018-03-28 21:06:52 -0300

    Adição controller Pessoa Física

---
+++ application/controllers/PessoaFisica.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ */
+class PessoaFisica extends CI_Controller
+{
+  public function index()
+  {
+
+  }
+
+  public function save()
+  {
+
+  }
+
+  public function update()
+  {
+
+  }
+
+  public function delete()
+  {
+
+  }
+
+}
+
+
+
+
+ ?>
