d8b763378a7e3ba650304f36f38893a6a935f9aa
Camila_Sales <camilapsales27@gmail.com>
2018-04-01 18:33:06 -0300

    Alteração models

--- application/models/Funcionario_model.php
+++ application/models/Funcionario_model.php
@@ -1,30 +1,30 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Cliente_model extends CI_Model {
+class Funcionario_model extends CI_Model {
 
 	public $id_pessoa_fisica;
 	/**
 	* @author: Camila Sales
-	* Salva o registro de cliente associado à uma pessoa fisica
+	* Salva o registro de funcionario associado à uma pessoa fisica
 	*
 	* @param integer $id_pessoa_fisica
 	*/
 	public function insert($id_pessoa_fisica)
 	{
 		$this->id_pessoa = $id_pessoa_fisica;
-		$this->db->insert('cliente', $this);
+		$this->db->insert('funcionario', $this);
 	}
 
 	/**
 	* @author: Camila Sales
-	* Remove o registro de cliente associado à uma pessoa fisica
+	* Remove o registro de funcionario associado à uma pessoa fisica
 	*
 	* @param integer $id_pessoa_fisica
 	*/
 	public function remove($id_pessoa_fisica)
 	{
 		$this->db->where('id_pessoa_fisica', $id_pessoa_fisica);
-		$this->db->delete('cliente');
+		$this->db->delete('funcionario');
 		// delete pessoa fisica;
 	}
 }
--- application/models/PessoaFisica_model.php
+++ application/models/PessoaFisica_model.php
@@ -1,19 +1,65 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class PessoaFisica extends CI_Model{
+class PessoaFisica_model extends CI_Model{
 
-  public $nome;
   public $email;
   public $dataNascimento;
   public $sexo;
+  public $id_pessoa;
 
-	public function __constructor(){
-		parent::__constructor();
+  /**
+	* @author: Camila Sales
+	* Este método tem como finalidade salvar um registro de pessoa
+	* física no banco
+	*
+	*/
+	public function insert()
+	{
+    // insert pessoa;
+    $this->email  = $this->input->post('email');
+		$this->$dataNascimento  = $this->input->post('data_nascimento');
+    $this->sexo = $this->input->post('sexo');
+		$this->id_pessoa = 1;
+    // $this->id_pessoa = $this->input->post('id_pessoa');
+
+		$this->db->insert('pessoa_fisica', $this);
+    return $this->db->insert_id();
 	}
-
-	public function get(){
-		$pessoa_fisica = $this->db->query("Select * from pessoa_fisica");
-		return $pessoa_fisica->result();
+	/**
+	* @author: Camila Sales
+	* Retorna todas as pessoas fisicas pessoas fisicas.
+	* @return mixed array de objetos
+	*/
+	public function get()
+	{
+		$this->db->select('*')->from('pessoa_fisica')->get()->result();
 	}
+	/**
+	* @author: Camila Sales
+	* Este método tem como finalidade atualizar os dados
+	* de um registro de pessoa no banco pelo id do mesmo.
+	*
+	*/
+	public function update()
+	{
+    // update pessoa
+    $this->email  = $this->input->post('email');
+    $this->$dataNascimento  = $this->input->post('data_nascimento');
+    $this->sexo = $this->input->post('sexo');
 
+		$this->db->update('pessoa_fisica', $this, array('id_pessoa_fisica' => $this->input->post('id_pessoa_fisica')));
+	}
+	/**
+	* @author: Camila Sales
+	* Este método tem como finalidade remover um registro de pessoa do banco
+	* pelo id do mesmo.
+	*
+	*/
+	public function remove()
+	{
+		$this->db->where('id_pessoa_fisica', $this->input->post('id_pessoa_fisica'));
+		$this->db->delete('pessoa');
+    // remove pessoa
+	}
 }
