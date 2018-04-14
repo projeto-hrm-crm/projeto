1b5d4e54dc30fc74c564385393298c0997d9aaa0
Rodrigo Alves <rodrigodeveloper1@gmail.com>
2018-04-14 11:38:05 -0300

    Models do SAC

---
+++ application/controllers/Sac.php
@@ -0,0 +1,28 @@
+<?php
+class Sac extends CI_Controller
+{
+
+  public function index(){
+
+  }
+
+  /**
+  * @author: Rodrigo Alves
+  * Este método tem como finalidade criar uma ordem de SAC.
+  *
+  */
+  public function save() {
+      
+  }
+
+  /**
+  * @author: Rodrigo Alves
+  * Este método tem como finalidade apagar uma ordem de SAC.
+  *
+  * @param integer $id_sac
+  */
+  public function delete($id_sac) {
+          
+  }
+
+}
