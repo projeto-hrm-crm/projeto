10b59f7839bb5bbdce625cd6461fbbda68e33693
matheusladislau <matheusladislauribeiro@gmail.com>
2018-04-14 11:57:58 -0300

    Criados métodos básicos

--- application/controllers/Setor.php
+++ application/controllers/Setor.php
@@ -1,5 +1,30 @@
 <?php
 class Setor extends CI_Controller
 {
+  public function index()
+  {
+    $data['title'] = 'Setores';
+    loadTemplate(
+      'includes/header',
+      'setor/setor',
+      'includes/footer',
+      $data
+    );
+  }
+
+  public function create()
+  {
+
+  }
+
+  public function edit()
+  {
+
+  }
+
+  public function delete()
+  {
+
+  }
 
 }
