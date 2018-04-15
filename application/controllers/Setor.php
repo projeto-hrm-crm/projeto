fc9a9ea9210e8226cc1b612ff9c5b32312417b3a
Matheus Ladislau Ribeiro <matheusladislauribeiro@gmail.com>
2018-04-15 15:51:50 -0300

    Método create de Controller setor implementado

--- application/controllers/Setor.php
+++ application/controllers/Setor.php
@@ -3,18 +3,23 @@ class Setor extends CI_Controller
 {
   public function index()
   {
+    $setores=$this->setor->get();
     $data['title'] = 'Setores';
-    loadTemplate(
-      'includes/header',
-      'setor/setor',
-      'includes/footer',
-      $data
-    );
+    loadTemplate('includes/header', 'setor/index', 'includes/footer', $data);
   }
+  // 
+  // public function teste(){
+  //   $this->load->view("setor/cadastrar");
+  // }
 
+  /**
+  * @author: Matheus Ladislau
+  *Realizar o cadastro de um setor, dados recebidos da view setor/cadastro.php
+  */
   public function create()
   {
-
+    $data["nome"]=$this->input->post("nome");
+    $this->setor->insert($data);
   }
 
   public function edit()
@@ -22,9 +27,13 @@ class Setor extends CI_Controller
 
   }
 
-  public function delete()
+  public function delete($id_setor)
   {
 
   }
+    //
+    // echo "<pre>";
+    // print_r($setores);
+    // echo "</pre>";
 
 }
