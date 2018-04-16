0fe4bd04adf299c21c3afa052c0a540f1c6c672f
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-04-16 14:59:32 -0300

    readequado ao padrão

--- application/controllers/Pessoa.php
+++ application/controllers/Pessoa.php
@@ -11,8 +11,8 @@ class Pessoa extends CI_Controller
 
 	/**
 	* @author: Tiago Villalobos
-	* Este método tem como finalidade validar os campos de pessoa, 
-	* e permitir que o model insira estes dados no banco, respeitando a ordem de inserção.
+	* Cria o formulário para cadastro de pessoa ou submete uma nova pessoa, caso seja enviada uma requisão POST.
+	* 
 	*
 	* 
 	*/
@@ -25,7 +25,12 @@ class Pessoa extends CI_Controller
 				$this->session->set_flashdata('errors', $this->form_validation->error_array());
 				$this->session->set_flashdata('old_values', $this->input->post());
 				
-				redirect('pessoa');
+				$this->session->set_flashdata(
+					'danger', 
+					'Não foi possível realizar o cadastro<br>Verifique os campos abaixo'
+				);
+
+				redirect('cadastrar/pessoa');
 			}
 			else
 			{
@@ -37,16 +42,19 @@ class Pessoa extends CI_Controller
 
 				$this->session->set_flashdata('success', 'Cadastro efetuado com sucesso');
 
-				redirect('pessoa');
+				redirect('cadastrar/pessoa');
 			}
 		}
 		else
 		{
-			$data['title']       = 'Cadastrar Pessoa';
-			$data['estados']     = $this->estado->get();
-			$data['errors']      = $this->session->flashdata('errors');
-			$data['old_values']  = $this->session->flashdata('old_values');
-			$data['message']     = $this->session->flashdata('success');
+			$data['title']         = 'Cadastrar Pessoa';
+			$data['estados']       = $this->estado->get();
+
+			//Cria váriaveis com os flashdata para facilitar a manipulação na view 
+			$data['errors']          = $this->session->flashdata('errors');
+			$data['old_values']      = $this->session->flashdata('old_values');
+			$data['success_message'] = $this->session->flashdata('success');
+			$data['error_message']   = $this->session->flashdata('danger');
 
 			$data['assets'] = array(
 				'css' => array('pessoa/style.css'),
@@ -66,37 +74,30 @@ class Pessoa extends CI_Controller
 		}
   	}
 
-
-
-	
-
-
 	/**
 	* @author: Tiago Villalobos
 	* Utiliza um id para buscar uma pessoa no banco e preencher o formulário de edição 
 	*
-	* @todo retornar a view com o formulário
-	* @param integer $id_pessoa
+	* 
+	* @param integer $id identificação da pessoa
+	* @todo mudar o formulário de edição
 	*/
-		/**
-	* @author: Tiago Villalobos
-	* Este método tem como finalidade validar os campos de pessoa, 
-	* e permitir que o model atualize este registro no banco.
-	*
-	* @todo criar o formulário e apresentar os erros
-	*/
-
 	public function edit($id)
 	{
 		
-		if($this->input->post)
+		if($this->input->post())
 		{
 			if(!$this->form_validation->run('pessoa'))
 			{
 				$this->session->set_flashdata('errors', $this->form_validation->error_array());
 				$this->session->set_flashdata('old_values', $this->input->post());
 
-				redirect('pessoa/edit/'.$id);
+				$this->session->set_flashdata(
+					'danger', 
+					'Não foi possível atualizar o cadastro<br>Verifique os campos abaixo'
+				);
+
+				redirect('editar/pessoa/'.$id);
 			}
 			else
 			{
@@ -107,38 +108,45 @@ class Pessoa extends CI_Controller
 
 				$this->session->set_flashdata('success', 'Cadastro editado com sucesso');
 
-				redirect('pessoa');
+				redirect('cadastrar/pessoa');
 			}
 		}
+		else
+		{
 
-		//TODO
-		$data['pessoa']      = $this->pessoa->find($id);
-		$data['title']       = 'Editar Cadastro';
-		$data['errors']      = $this->session->flashdata('errors');
-		$data['old_values']  = $this->session->flashdata('old_values');
-		$data['message']     = $this->session->flashdata('success');
+			$data['pessoa']        = $this->pessoa->getById($id);
+			$data['title']         = 'Editar Cadastro';
+			$data['estados']       = $this->estado->get();
 
-		$data['assets'] = array(
-			'css' => array('pessoa/style.css'),
-			'js' => array(
-				'lib/jquery/jquery.mask.min.js',
-				'pessoa/main.js',
-				'pessoa/validate-form.js',
-			),
-		);
+			//Cria váriaveis com os flashdata para facilitar a manipulação na view 
+			$data['errors']          = $this->session->flashdata('errors');
+			$data['old_values']      = $this->session->flashdata('old_values');
+			$data['success_message'] = $this->session->flashdata('success');
+			$data['error_message']   = $this->session->flashdata('danger');
 
-		loadTemplate(
-			'includes/header',
-			'pessoa/create-form',
-			'includes/footer',
-			$data
-		);
+			$data['assets'] = array(
+				'css' => array('pessoa/style.css'),
+				'js' => array(
+					'lib/jquery/jquery.mask.min.js',
+					'pessoa/main.js',
+					'pessoa/validate-form.js',
+				),
+			);
+
+			//Mudar para o formulário de edição
+			loadTemplate(
+				'includes/header',
+				'pessoa/create-form',
+				'includes/footer',
+				$data
+			);
+			
+		}
+
+		
 
 		
 	}
-
-
-
 
 	/**
 	* @author: Tiago Villalobos
@@ -149,16 +157,21 @@ class Pessoa extends CI_Controller
 	*/
 	public function delete($id)
 	{
-		
 		$this->telefone->remove($id);
 		$this->documento->remove($id);
 		$this->endereco->remove($id);
 
-		$this->pessoa->remove();
+		if($this->pessoa->remove($id))
+		{
+			$this->session->set_flashdata('success', 'Cadastro removido com sucesso!');
+		}
+		else
+		{
+			$this->session->set_flashdata('danger', 'Não foi possível remover o cadastro!');	
+		}
 
-		$this->session->set_flashdata('success', 'Cadastro removido com sucesso');
 
-		redirect('pessoa');
+		redirect('cadastrar/pessoa');
 	}
 
 }
