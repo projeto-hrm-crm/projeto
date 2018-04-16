972b08d06f39832da4f250cbedf84db320c72b40
Matheus Ladislau Ribeiro <matheusladislauribeiro@gmail.com>
2018-04-16 19:51:51 -0300

    Métodos comentados, implementados métodos update, remove de Model Setor_model.php

--- application/models/Setor_model.php
+++ application/models/Setor_model.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Setor_model extends CI_Model
 {
   public $nome;
+  public $id_setor;
 
   public function __construct(){
       parent::__construct();
@@ -12,7 +13,7 @@ class Setor_model extends CI_Model
   /**
   * @author: Matheus Ladislau
   * Retorna todos registro de setor cadastrados no banco
-  *@return
+  * @return array: todos registro de setor cadastrados
   */
   public function get(){
       $query = $this->db->get('setor');
@@ -21,14 +22,37 @@ class Setor_model extends CI_Model
 
   /**
   * @author: Matheus Ladislau
-  * Registra setor
+  * Realiza registro de setor
   *
-  * @param integer $id_pessoa
+  *@param array: Dados (id_setor,nome) a serem registrados
   */
   public function insert($data)
   {
     $this->db->insert('setor',$data);
   }
 
+  /**
+  * @author: Matheus Ladislau
+  * Edita o registro de setor pelo id_setor referente
+  *
+  * @param integer $id_setor refere-se ao id do registro de setor a ser editado 
+  * @return boolean: True - caso editado com sucesso, False - não editado
+  */
+  public function update($data,$id_setor)
+  {
+    return $this->db->update("setor",$data,array('id_setor'=>$id_setor));
+  }
+
+  /**
+	* @author: Matheus Ladislau
+	* Remove o registro de setor pelo id_setor referente
+	*
+	* @param integer: $id_setor refere-se ao id do registro de setor a ser deletado
+	*/
+	public function remove($id_setor)
+	{
+		$this->db->where('id_setor', $id_setor);
+		$this->db->delete('setor');
+	}
 }
 ?>
