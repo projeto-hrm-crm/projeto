a69f00be1b255672be88164fbabd4c5f991576e7
Níkolas Lencioni <nikolasagl@hotmail.com>
2018-04-18 23:45:08 -0300

    Update PessoaJuridica_model.php

--- application/models/PessoaJuridica_model.php
+++ application/models/PessoaJuridica_model.php
@@ -3,15 +3,55 @@
 class PessoaJuridica_model extends CI_Model
 {
 
-  public function get(){}
+  public function get()
+  {
+    try {
+      $query = $this->db->get('pessoa_juridica');
+      if ($query)
+      {
+        return $query->result();
+      }else{
+        echo 'Não existem dados';
+        exit;
+      }
+    } catch (\Exception $e) {}
+  }
 
-  public function insert(){}
+  public function insert($data)
+  {
+		$this->db->insert('pessoa_juridica', $data);
+		return $this->db->insert_id();
+  }
 
-  public function find($id){}
+  public function find($id)
+  {
+    try {
+      $pessoa_juridica = $this->db->select('*')->from('pessoa_juridica')->where('id', $id)->get();
+      if ($pessoa_juridica)
+      {
+        return $pessoa_juridica->result();
+      }else{
+        echo 'Pessoa Juridica não existe';
+        return 1;
+      }
+    } catch (\Exception $e) {}
+  }
 
-  public function update($id){}
+  public function update($id, $data)
+  {
+    try {
+      $this->db->where('id', $id);
+      $this->db->update('pessoa_juridica', $data);
+    } catch (\Exception $e) {}
+  }
 
-  public function delete($id){}
+  public function delete($id)
+  {
+    try {
+      $this->db->where('id', $id);
+      $this->db->delete('pessoa_juridica');
+    } catch (\Exception $e) {}
+  }
 
 
 }
