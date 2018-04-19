6a87c37f5863d001fdb17bafced7f2ac92819800
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-04-19 15:28:52 -0300

    view cadastro de vaga criada

---
+++ application/views/vaga/cadastrar.php
@@ -0,0 +1,48 @@
+
+<div class="animated fadeIn">
+	<div class="row justify-content-center align-items-center">
+<div class="col-lg-8">
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">Nova Vaga</strong>
+            </div>
+            <form action="<?php echo base_url('cadastrar/produto'); ?>" method="post" novalidate="novalidate">
+                <div class="card-body">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="form-group col-6">
+                                <label for="id_cargo" class="control-label mb-1">Cargo</label>
+                                <select name="id_cargo" id="id_cargo" class="form-control">
+	                                <option value="">Selecione</option>
+	                                <?php foreach ($cargos as $cargo): ?>
+	                                	<option value="<?php echo $cargo->id_cargo ?>" <?php echo isset($old_data['id_cargo']) && $cargo->id_cargo == $old_data['id_cargo'] ? 'selected' : '' ?>>
+	                                		<?php echo $cargo->nome ?>
+	                                	</option>
+	                                <?php endforeach; ?>
+                              	</select>
+                             	<span class="invalid-feedback"></span>
+                            </div>
+                            <div class="form-group col-6">
+                                <label for="data_oferta" class="control-label mb-1">Data da Oferta</label>
+                                <input id="data_oferta" value="<?php echo isset($old_data['data_oferta']) ? $old_data['data_oferta'] : null;?>" name="data_oferta" type="text" class="data form-control <?php echo isset($errors['data_oferta']) ? 'is-invalid' : '' ?>" required>
+                                <span class="invalid-feedback"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-right">
+                    <button type="reset" class="btn btn-danger btn-sm">
+                        <i class="fa fa-ban"></i>
+                        Apagar
+                    </button>
+                    <button type="submit" class="btn btn-primary btn-sm">
+                        <i class="fa fa-check"></i>
+                        Cadastrar
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</div>
+</div>
