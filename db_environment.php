eb5655c01d39d0dd605952fc0ecfb94415751b1e
pigozzi <rafaelpigozzi@gmail.com>
2018-04-18 21:09:03 -0300

    Alteração do banco para TESTE

--- db_environment.php
+++ db_environment.php
@@ -10,9 +10,4 @@ $database_config = [
     'host'      => 'localhost',
     'user'		=> 'root',
     'password' 	=> 'aluno123',
-<<<<<<< HEAD
-    'database'	=> 'database',
-=======
     'database'	=> 'teste',
->>>>>>> f64a703db3cdf4acf2f38f1778c96d790fd31227
-];
