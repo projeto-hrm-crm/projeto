b00669b8dcc9cf61e703f82825aec06b0c9df445
Níkolas Lencioni <nikolasagl@hotmail.com>
2018-04-18 23:44:30 -0300

    Update editar.php

--- application/views/fornecedor/editar.php
+++ application/views/fornecedor/editar.php
@@ -23,20 +23,43 @@
       <form action="<?php site_url('fornecedor/edit'.$id); ?>" method="POST" class="form-horizontal">
         <div class="row form-group">
           <div class="col col-md-3"><label class=" form-control-label">Nome</label></div>
-          <div class="col-12 col-md-9"><input type="text" id="nome" name="nome" class="form-control" value="<?= htmlspecialchars($fornecedor[0]->nome)?>"></div>
+          <div class="col-12 col-md-9"><input type="text" id="nome" name="nome" placeholder="Nome" class="form-control"></div>
         </div>
         <div class="row form-group">
-          <div class="col col-md-3"><label for="email-input" class=" form-control-label">Pessoa Juridica</label></div>
-          <div class="col-12 col-md-9"><input type="text" id="id_pessoa_juridica" name="id_pessoa_juridica" value="<?= htmlspecialchars($fornecedor[0]->id_pessoa_juridica)?>" class="form-control"></div>
+          <div class="col col-md-3"><label for="email-input" class=" form-control-label">E-mail</label></div>
+          <div class="col-12 col-md-9"><input type="email" id="email" name="email" placeholder="e-mail" class="form-control"></div>
         </div>
         <div class="row form-group">
           <div class="col col-md-3"><label class=" form-control-label">Razão Social</label></div>
-          <div class="col-12 col-md-9"><textarea name="razao_social" id="razao_social" rows="9" class="form-control"><?= htmlspecialchars($fornecedor[0]->razao_social)?></textarea></div>
+          <div class="col-12 col-md-9"><textarea name="razao_social" id="razao_social" rows="9" placeholder="Conteudo..." class="form-control"></textarea></div>
         </div>
         <div class="row form-group">
           <div class="col col-md-3"><label class=" form-control-label">CNPJ</label></div>
-          <div class="col-12 col-md-9"><input type="text" id="cnpj" name="cnpj" value="<?= htmlspecialchars($fornecedor[0]->cnpj)?>" class="form-control"></div>
+          <div class="col-12 col-md-9"><input type="text" id="cnpj" name="cnpj" placeholder="CNPJ" class="form-control"></div>
         </div>
+		<div class="row form-group">
+          <div class="col col-md-3"><label class=" form-control-label">Telefone</label></div>
+          <div class="col-12 col-md-9"><input type="text" id="telefone" name="telefone" placeholder="(12)3889-9090" class="form-control"></div>
+        </div>
+		<div class="card-header">
+			<strong>Endereço</strong>
+		</div><br>
+		<div class="row form-group">
+          <div class="col col-md-3"><label class=" form-control-label">Estado</label></div>
+          <div class="col-12 col-md-9"><input type="text" id="estado" name="estado" placeholder="São Paulo" class="form-control"></div>
+        </div>
+		<div class="row form-group">
+          <div class="col col-md-3"><label class=" form-control-label">Cidade</label></div>
+          <div class="col-12 col-md-9"><input type="text" id="cidade" name="cidade" placeholder="Caraguatuba" class="form-control"></div>
+        </div>
+		<div class="row form-group">
+          <div class="col col-md-3"><label class=" form-control-label">Bairro</label></div>
+          <div class="col-12 col-md-9"><input type="text" id="bairro" name="bairro" placeholder="Bairro" class="form-control"></div>
+        </div>
+		<div class="row form-group">
+          <div class="col col-md-3"><label class=" form-control-label">Número</label></div>
+          <div class="col-12 col-md-9"><input type="num" id="numero" name="numero" placeholder="CNPJ" class="form-control"></div>
+    </div>
         <div class="card-footer">
           <button type="submit" class="btn btn-primary btn-sm">
             <i class="fa fa-dot-circle-o"></i> Enviar
