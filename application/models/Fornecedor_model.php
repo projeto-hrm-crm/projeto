ef2b286ea048e3732c53bc4037398654eea66887
Níkolas Lencioni <nikolasagl@hotmail.com>
2018-04-18 23:44:51 -0300

    Update Fornecedor_model.php

--- application/models/Fornecedor_model.php
+++ application/models/Fornecedor_model.php
@@ -6,13 +6,19 @@ class Fornecedor_model extends CI_Model
   public function get()
   {
     try {
-      $query = $this->db->get('fornecedor');
+      $query = $this->db->select('fornecedor.id_fornecedor, pessoa_juridica.id_pessoa_juridica, pessoa_juridica.razao_social, pessoa.id_pessoa, pessoa.nome, pessoa.email')//, telefone.numero AS telefone, documento.numero AS cnpj, endereco.cep, endereco.estado, endereco.cidade, endereco.bairro, endereco.logradouro, endereco.numero AS numero_endereco, endereco.complemento')
+  		->from('fornecedor')
+      ->join('pessoa_juridica', 'pessoa_juridica.id_pessoa_juridica = fornecedor.id_pessoa_juridica')
+      ->join('pessoa', 'pessoa.id_pessoa = pessoa_juridica.id_pessoa')
+  		// ->join('telefone', 'telefone.id_pessoa = pessoa.id_pessoa')
+  		// ->join('documento', 'documento.id_pessoa = pessoa.id_pessoa')
+  		// ->join('endereco', 'endereco.id_pessoa = pessoa.id_pessoa')
+  		->get();
       if ($query)
       {
         return $query->result();
       }else{
-        echo 'Não existem dados';
-        exit;
+        return 0;
       }
     } catch (\Exception $e) {}
   }
@@ -20,8 +26,40 @@ class Fornecedor_model extends CI_Model
 
   public function insert($data)
   {
+    $pessoa['nome'] = $data['nome'];
+    $pessoa['email'] = $data['email'];
+
+    // $documento['numero'] = $data['cnpj'];
+    // $documento['tipo'] = 2;
+    //
+    // $telefone['telefone'] = $data['telefone'];
+    //
+    // $endereco['cep'] = $data['cep'];
+    // $endereco['estado'] = $data['estado'];
+    // $endereco['cidade'] = $data['cidade'];
+    // $endereco['bairro'] = $data['bairro'];
+    // $endereco['logradouro'] = $data['logradouro'];
+    // $endereco['numero'] = $data['numero'];
+    // $endereco['complemento'] = $data['complemento'];
+
+    $pessoa_juridica['razao_social'] = $data['razao_social'];
+
     try {
-      $this->db->insert('fornecedor', $data);
+      $id = $this->pessoa->insert($pessoa);
+      // $documento['id_pessoa'] = $id;
+      // $telefone['id_pessoa'] = $id;
+      // $endereco['id_pessoa'] = $id;
+      $pessoa_juridica['id_pessoa'] = $id;
+
+      // $this->Documento->insert($documento);
+      // $this->Telefone->insert($telefone);
+      // $this->Endereco->insert($endereco);
+      $id2['id_pessoa_juridica'] = $this->pessoa_juridica->insert($pessoa_juridica);
+      // print_r($id2);
+      // exit();
+
+      $this->db->insert('fornecedor', $id2);
+      return $this->db->insert_id();
     } catch (\Exception $e) {}
   }
 
