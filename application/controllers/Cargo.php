193ffabe3558d9cc9a13aa50b3c3afcc3e28770f
Matheus Ladislau Ribeiro <matheusladislauribeiro@gmail.com>
2018-04-22 14:41:41 -0300

    Criado controller de Cargo: Cargo.php

---
+++ application/controllers/Cargo.php
@@ -0,0 +1,9 @@
+<?php
+class Cargo extends CI_Controller
+{
+  public function index()
+  {
+    // $setores=$this->setor->get();
+    // $data['title'] = 'Setores';
+    // loadTemplate('includes/header', 'setor/index', 'includes/footer', $data);
+  }
