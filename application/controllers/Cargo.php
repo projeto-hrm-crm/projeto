e40203bc69dd9d6a87514c67042c1a04b202766f
Matheus Ladislau Ribeiro <matheusladislauribeiro@gmail.com>
2018-04-22 16:02:58 -0300

    Criado métodos crud de controller cargo

--- application/controllers/Cargo.php
+++ application/controllers/Cargo.php
@@ -3,7 +3,47 @@ class Cargo extends CI_Controller
 {
   public function index()
   {
-    // $setores=$this->setor->get();
-    // $data['title'] = 'Setores';
-    // loadTemplate('includes/header', 'setor/index', 'includes/footer', $data);
+    $cargos=$this->cargo->get();
+    $data['title'] = 'Cargos';
+    loadTemplate('includes/header', 'cargo/index', 'includes/footer', $data);
   }
+
+
+  /**
+  * @author: Matheus Ladislau
+  * Realiza o cadastro de um cargo, dados recebidos da view cargo/cadastro.php
+  */
+  public function create()
+  {
+    $data["nome"]=$this->input->post("nome");
+    $data["descricao"]=$this->input->post("descricao");
+    $data["id_setor"]=$this->input->post("id_setor");
+    $this->cargo->insert($data);
+  }
+
+  /**
+  * @author: Matheus Ladislau
+  * Realiza edição de registro de um cargo pelo id, dados recebidos pela view cargo/editar.php
+  *
+  * @param integer: referem-se ao id do cargo a ser alterado
+  */
+  public function edit($id_cargo)
+  {
+    $data["nome"]=$this->input->post("nome");
+    $data["descricao"]=$this->input->post("descricao");
+    $data["id_setor"]=$this->input->post("id_setor");
+    $this->cargo->update($id_cargo,$data);
+  }
+
+  /**
+  * @author: Matheus Ladislau
+  * Realiza remoção de registro de um cargo pelo id, dados recebidos pela view cargo/delete.php
+  *
+  * @param integer: refere-se ao id do cargo a ser alterado
+  */
+  public function delete($id_cargo)
+  {
+    $this->cargo->remove($id_cargo);
+  }
+}
+?>
