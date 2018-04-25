3a3a90f931f514469170af26bd9edf9e77bce7e6
Pedro Henrique Guimarães <pedro.guimaraes@alboompro.com.br>
2018-04-25 01:36:46 -0300

    Finalização do processo de grupo de acessos, talvez algumas alterações precisem ser feitas ao longo do projeto

---
+++ application/models/Grupo_model.php
@@ -0,0 +1,5 @@
+<?php 
+class Grupo_model extends CI_Model
+{
+    
+}
--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -25,7 +25,7 @@ class Usuario_model extends CI_Model
             $sql = $this->db->get();
 
             if ($sql->num_rows() > 0) {
-                $this->session->userdata('user_login', $sql->result()[0]->id_usuario);
+                $this->session->set_userdata('user_login', $sql->result()[0]->id_usuario);
                 return true; 
             }
             return false; 
@@ -34,15 +34,47 @@ class Usuario_model extends CI_Model
 
     /**
      * @author Pedro Henrique Guimarães
-     * Verifica se o usuário esta logado 
+     * Método responsável por buscar o grupo de acesso pelo id_usuario 
      * 
-     * @return boolean
+     * @param $user_id
+     * @return int|bool
      */
-    public function isLoggedIn()
+    public function getUserAccessGroup($user_id)
     {
-        if ($this->session->userdata('usuario') != null) 
-            return true; 
+        $this->db->select('id_grupo_acesso')
+                 ->from('usuario')
+                 ->where('id_usuario', $user_id);
+        $result = $this->db->get();
+
+        if ($result->num_rows() > 0) 
+            return $result->result()[0]->id_grupo_acesso;
         return false;
     }
 
+    /**
+     * @author Pedro Henrique Guimarães
+     * Verifica se o usuário tem permissão para acessar determinada página 
+     * baseado no seu grupo de permissão
+     * 
+     * @param $user_id
+     * @return void|false
+     */
+    public function hasPermission($user_id, $url)
+    {
+        if (!empty($url)) {
+            $access_group = $this->getUserAccessGroup($user_id);
+            if ($user_id) {
+                $this->db->select('id_grupo_acesso')
+                        ->from('categoria as c')
+                        ->join('subcategoria as s', 's.id_categoria = c.id_categoria')
+                        ->where('s.link', $url);
+                $result = $this->db->get();
+                if (!$result->num_rows() > 0 || $result->result()[0]->id_grupo_acesso != $access_group) {
+                    redirect(base_url());
+                }
+            } else {
+                redirect(base_url());
+            } 
+        }   
+    }
 }
