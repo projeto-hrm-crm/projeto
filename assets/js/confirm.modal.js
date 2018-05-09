9e5ced10b122b1a94a57ca75f3b53778823677ca
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-05-09 15:50:37 -0300

    js para o modal de remoção

--- assets/js/confirm.modal.js
+++ assets/js/confirm.modal.js
@@ -1,28 +1,7 @@
 $(document).ready(function() {
 
-	function setConfirmModalEvent(type, form = null)
-	{
-		if(type === 'edit')
-		{
-			$('.btn-edit').click(function(event){
-		        event.preventDefault();
-			});
-
-			$('.btn-edit-ok').click(function(){
-				form.submit();
-			});	
-		}
+	$('#modalRemover').on('show.bs.modal', function(event) {
+	    $(this).find('.btn-remove-ok').attr('href', $(event.relatedTarget).data('href'));
+	});
 		
-		if(type === 'remove')
-		{
-			$('#modalRemover').on('show.bs.modal', function(e) {
-			    $(this).find('.btn-remove-ok').attr('href', $(e.relatedTarget).data('href'));
-			});	
-		}
-	}
-
-
-	setConfirmModalEvent('edit', $('#form-vaga'));
-	setConfirmModalEvent('remove');
-	
 });
