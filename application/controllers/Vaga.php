47e73e015f4b7641e79e6d49773bb2d4e158220c
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-05-09 12:23:12 -0300

    carregando js do modal

--- application/controllers/Vaga.php
+++ application/controllers/Vaga.php
@@ -37,6 +37,7 @@ class Vaga extends CI_Controller
           'lib/data-table/datatables.min.js',
           'lib/data-table/dataTables.bootstrap.min.js',
           'datatable.js',
+          'confirm.modal.js'
         ),
       );
       
@@ -133,8 +134,8 @@ class Vaga extends CI_Controller
         $data['old_data'] = $this->session->flashdata('old_data');
        $data['assets'] = array(
         'js' => array(
-          
           'validate.js',
+          'confirm.modal.js'
         ),
 
       );
