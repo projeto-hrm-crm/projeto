97837a33c2fbe0f0e4032e7a666421c57e078c41
Camila_Sales <camilapsales27@gmail.com>
2018-05-11 16:42:34 -0300

    add mascaras

--- assets/js/mask.js
+++ assets/js/mask.js
@@ -2,5 +2,8 @@ $(document).ready(function($) {
     (function($){
         $('.data').mask('00/00/0000');
         $('.telefone').mask('(00)00000-0000');
+        $('.cep').mask('00000-000');
+        $('.cpf').mask('000.000.000-00');
+
     })(jQuery);
 });
