5b4e2e272077360af050b3d9546cc7b10fcbeda9
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-05-15 15:12:17 -0300

    rotas para pedido

--- application/config/routes.php
+++ application/config/routes.php
@@ -123,3 +123,10 @@ $route['vaga']                = 'Vaga/index';
 $route['vaga/cadastrar']      = 'Vaga/create';
 $route['vaga/editar/(:num)']  = 'Vaga/edit/$1';
 $route['vaga/excluir/(:num)'] = 'Vaga/delete/$1';
+
+
+//Rotas Pedido
+$route['pedido']                = 'Pedido/index';
+$route['pedido/cadastrar']      = 'Pedido/create';
+$route['pedido/editar/(:num)']  = 'Pedido/edit/$1';
+$route['pedido/excluir/(:num)'] = 'Pedido/delete/$1';
