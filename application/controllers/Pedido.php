f83d19c854fe87dc48d4824efd6098be25065c91
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-05-16 16:53:44 -0300

    metodo create funcionando

--- application/controllers/Pedido.php
+++ application/controllers/Pedido.php
@@ -1,18 +1,21 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Pedido extends CI_Controller {
+class Pedido extends CI_Controller 
+{
 
 	/**
-	* @author Tiago Villalobos
-	* Lista com os pedidos
-	*
-	*
-	*/
-	public function index()
-	{
-		
-	}
+	 * @author Pedro Henrique Guimarães
+	 * Com a configuração do menu esse controller serve como base para todos os outros controllers
+	 * onde todos devem seguir essa mesma estrutura mínima no consrutor.
+	 */
+	// public function __construct()
+	// {
+	//   	parent::__construct();
+	//     $user_id = $this->session->userdata('user_login');
+	//     $currentUrl = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+	//     $this->usuario->hasPermission($user_id, $currentUrl);
+	// }
 
 
 	/**
@@ -24,22 +27,55 @@ class Pedido extends CI_Controller {
   	{
   		if($this->input->post())
   		{
-  			print_r($this->input->post());
+  			if($this->form_validation->run('pedido'))
+  			{
+  				$pedido = array(
+  					'id_cliente' => $this->input->post('id_cliente'),
+  					'descricao'  => $this->input->post('descricao'),
+  					'tipo'       => $this->input->post('tipo')
+  				);
+
+  				$id_pedido = $this->pedido->insert($pedido);
+
+  				$andamento = array(
+  					'data'      => date('Y-m-d H:i:s'),
+  					'situacao'  => $this->input->post('situacao'),
+  					'id_pedido' => $id_pedido
+  				);
+
+  				$this->andamento->insert($andamento);
+
+  				foreach($this->input->post('id_produto') as $index => $id_produto)
+  				{
+	  				$pedidoProduto = array(
+	  					'id_pedido'  => $id_pedido,
+	  					'id_produto' => $id_produto,
+	  					'quantidade' => $this->input->post('qtd_produto')[$index]
+	  				);
+
+	  				$this->pedido->insertProducts($pedidoProduto);
+  					
+  				}
+
+  			}
+  			else
+  			{
+  				$this->session->set_flashdata('errors', $this->form_validation->error_array());
+	            $this->session->set_flashdata('old_data', $this->input->post());
+	            redirect('pedido/cadastrar');
+  			}
   		}
   		else
   		{
-	  		$data['title']    = 'Pedido';
+	  		$data['title'] = 'Pedido';
+
 	  		$data['clientes'] = $this->cliente->get();
 	  		$data['produtos'] = $this->produto->get();
 
-	  		$data['assets'] = array(
-	  			'css' => array(
-	  				'lib/chosen/chosen.css'
-	  			),
-	  			'js' => array(
-					'lib/chosen/chosen.jquery.min.js'
-	  			),
-	  		);
+	  		$data['errors']          = $this->session->flashdata('errors');
+	  		$data['success_message'] = $this->session->flashdata('success');
+	  		$data['error_message']   = $this->session->flashdata('danger');
+	  		$data['old_data']        = $this->session->flashdata('old_data');
 
 			loadTemplate(
 				'includes/header',
@@ -52,32 +88,5 @@ class Pedido extends CI_Controller {
 
 
   	}
-
-	/**
-	* @author: Tiago Villalobos
-	* Utiliza um id para buscar uma pessoa no banco e preencher o formulário de edição
-	* Retorna messagem de erro ou sucesso por flashdata que são inseridos em variáveis para facilitar o
-	* trabalho na view.
-	*
-	* @param integer $id identificação da pessoa
-	*/
-	public function edit($id_pessoa)
-	{
-
-		
-
-	}
-
-	/**
-	* @author: Tiago Villalobos
-	* Este método tem como finalidade remover um registro de pessoa do banco com base no id passado.
-	* Seguindo a ordem de dependência.
-	*
-	* @param integer $id_pessoa
-	*/
-	public function delete($id_pessoa)
-	{
-		
-	}
 
 }
