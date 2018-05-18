186cf256f25f114d52a6b19a90536fcd91b3ba28
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-05-18 15:28:53 -0300

    remoção de andamento

--- application/models/Andamento_model.php
+++ application/models/Andamento_model.php
@@ -9,4 +9,25 @@ class Andamento_model extends CI_Model
 		$this->db->insert('andamento', $andamento);
 	}
 
+	public function update($andamento)
+	{
+        $this->db->where('andamento.id_pedido', $andamento['id_pedido']);
+        
+        $this->db->set('andamento.data', $andamento['data']);
+        $this->db->set('andamento.situacao', $andamento['situacao']);
+        
+        $this->db->update('andamento');
+
+    }
+
+    public function remove($id)
+    {
+        
+        $query = $this->db->where('andamento.id_pedido', $id);
+        
+        $query->delete('andamento');
+        
+        return $query->affected_rows() > 0 ? true : false;
+    }
+
 }
