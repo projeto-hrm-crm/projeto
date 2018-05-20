92c66bd06abaa6b6be422c8cfbf488f6b3772dda
Camila_Sales <camilapsales27@gmail.com>
2018-05-20 19:40:55 -0300

    removido js na view de edição

--- application/views/cliente/editar.php
+++ application/views/cliente/editar.php
@@ -115,15 +115,3 @@
     </div>
   </div>
 </div>
-
-<script src="<?= base_url('assets/js/lib/data-table/datatables.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/dataTables.bootstrap.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/dataTables.buttons.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/buttons.bootstrap.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/jszip.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/pdfmake.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/vfs_fonts.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/buttons.html5.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/buttons.print.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/buttons.colVis.min.js');?>"></script>
-<script src="<?= base_url('assets/js/lib/data-table/datatables-init.js');?>"></script>
