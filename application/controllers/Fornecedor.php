a5a9af852938df1709f970a32a6f3b6c850e00c6
BetoCadilhe <carlos.cadilhe@gmail.com>
2018-05-22 12:41:29 -0300

    correção da função delete

--- application/controllers/Fornecedor.php
+++ application/controllers/Fornecedor.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 * author: Nikolas Lencioni
 * Controller de fornecedor
 **/
-
 class Fornecedor extends CI_Controller
 {
    /**
@@ -17,9 +16,9 @@ class Fornecedor extends CI_Controller
       parent::__construct();
        $user_id = $this->session->userdata('user_login');
        $currentUrl = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
-       $this->usuario->hasPermission($user_id, $currentUrl);       
+       $this->usuario->hasPermission($user_id, $currentUrl);
+       $this->menus = $this->menu->getUserMenu($user_id);
    }
-
   /**
   * author: Nikolas Lencioni
   * Metodo index que chama a view inicial de fornecedores
@@ -38,11 +37,8 @@ class Fornecedor extends CI_Controller
    );
     // print_r($data);
     // exit();
-
     loadTemplate('includes/header', 'fornecedor/index', 'includes/footer', $data);
   }
-
-
   /**
   * author: Nikolas Lencioni
   * Metodo create, apresenta o formulario de cadastro, recebe os dados
@@ -54,24 +50,19 @@ class Fornecedor extends CI_Controller
   **/
   public function create()
   {
-
     $data = $this->input->post();
-
     if($data)
     {
       if ($this->form_validation->run('fornecedor'))
       {
         $this->fornecedor->insert($data);
         $this->session->set_flashdata('success', 'Fornecedor cadastrado com sucesso.');
-
         redirect('fornecedor');
       }else{
         $this->session->set_flashdata('danger', 'Fornecedor não pode ser cadastrado');
-
         redirect('fornecedor');
       }
     }
-
     $data['title'] = 'Cadastrar Fornecedor';
     $data['fornecedor'] = $this->input->post();
     $data['estados'] = $this->estado->get();
@@ -85,8 +76,6 @@ class Fornecedor extends CI_Controller
    );
     loadTemplate('includes/header', 'fornecedor/cadastrar', 'includes/footer', $data);
   }
-
-
   /**
   * author: Nikolas Lencioni
   * Metodo edit, apresenta o formulario de edição, com os dados do fornecedor a ser editado,
@@ -97,32 +86,29 @@ class Fornecedor extends CI_Controller
   *
   * @param $id int, id do fornecedor
   **/
-  public function edit($id_fornecedor)
+  public function edit($id)
   {
-
     $data = $this->input->post();
     if ($data)
     {
-
       if ($this->form_validation->run('fornecedor'))
       {
-        $this->fornecedor->update($id_fornecedor, $data);
+        $this->fornecedor->update($id, $data);
         $this->session->set_flashdata('success', 'Fornecedor editado com sucesso.');
         redirect('fornecedor');
       }else{
         $this->session->set_flashdata('danger', 'Fornecedor não pode ser atualizado.');
-        redirect('fornecedor/edit/'.$id_fornecedor);
+        redirect('fornecedor/edit/'.$id);
       }
     }
-
-    $fornecedor = $this->fornecedor->getById($id_fornecedor); //FIXME ARRUMAR
+    $fornecedor = $this->fornecedor->find($id); //FIXME ARRUMAR
     $state = $this->cidade->findState($fornecedor[0]->id_cidade);
-    $data['fornecedor'] = $this->fornecedor->find($id_fornecedor);
+    $data['fornecedor'] = $this->fornecedor->find($id);
     $data['title'] = 'Editar Fornecedor';
     $data['estados'] = $this->estado->get();
     $data['estado_fornecedor'] = $state;
     $data['cidades'] = $this->cidade->getByState($state[0]->id_estado);
-    $data['id'] = $id_fornecedor;
+    $data['id'] = $id;
     $data['assets'] = array(
      'js' => array(
        'lib/data-table/datatables.min.js',
@@ -131,41 +117,24 @@ class Fornecedor extends CI_Controller
        'confirm.modal.js',
      ),
    );
-
     loadTemplate('includes/header', 'fornecedor/editar', 'includes/footer', $data);
   }
-
   /**
   * author: Nikolas Lencioni
   * Metodo delete, chama a funçao delete de Fornecedor_model, passando o id do fornecedores
   * Redireciona para a pagina index de fornecedor
   *
-  * @param $id_fornecedor int
+  * @param $id int
   **/
-  /* public function delete($id_fornecedor)
+  public function delete($id)
   {
-     $fornecedor = $this->fornecedor->find($id_fornecedor);
+     $fornecedor = $this->fornecedor->find($id);
      if($fornecedor){
-        $this->fornecedor->delete($id_fornecedor);
+        $this->fornecedor->delete($id);
         $this->session->set_flashdata('success', 'fornecedor deletado com sucesso.');
      }else{
        $this->session->set_flashdata('danger', 'Impossível Deletar!');
      }
      redirect('fornecedor');
-
-  } */
-
-  public function delete($id_fornecedor)
-  {
-    $data['fornecedor'] = $this->fornecedor->getById($id_fornecedor);
-    if ($data)
-    {
-      $this->fornecedor->remove($id_fornecedor);
-      $this->session->set_flashdata('success', 'fornecedor excluido com sucesso');
-      redirect('fornecedor');
-    }
   }
-
-
-
 }
