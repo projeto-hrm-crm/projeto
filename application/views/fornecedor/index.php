ab8369bb6124f9690192ac6d8cd63494d3d837cc
BetoCadilhe <carlos.cadilhe@gmail.com>
2018-05-22 12:50:12 -0300

    titulo de modal

--- application/views/fornecedor/index.php
+++ application/views/fornecedor/index.php
@@ -75,7 +75,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Excluir produto</h5>
+                <h5 class="modal-title">Excluir Fornecedor</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
