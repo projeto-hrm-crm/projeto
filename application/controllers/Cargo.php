3bf32a56e25fa17ea54ea6367e0259f52b92d8d1
BetoCadilhe <carlos.cadilhe@gmail.com>
2018-05-29 14:47:59 -0300

    cargo

--- application/controllers/Cargo.php
+++ application/controllers/Cargo.php
@@ -137,32 +137,6 @@ class Cargo extends CI_Controller
   * @param integer: referem-se ao id do cargo a ser alterado
   */
 
-<<<<<<< HEAD
-  public function edit($id_cargo) {
-    if ($this->input->post()){
-
-            if($this->form_validation->run('cargo')){ 
-
-            $data["nome"]=$this->input->post("nome");
-            $data["descricao"]=$this->input->post("descricao");
-            $data["salario"]=$this->input->post("salario");
-            $data["id_setor"]=$this->input->post("id_setor");
-          /* $cargo = array(
-              'nome' => $this->input->post('nome'),
-              'descricao' => $this->input->post('descricao'),
-              'salario' => $this->input->post('salario'),
-              'id_setor' => $this->input->post('id_setor'),
-            ); */
-            $this->cargo->update($id_cargo,$data);
-            $this->session->set_flashdata('success', 'Cargo editado com sucesso');
-            redirect('cargo');
-          }else{
-            $this->session->set_flashdata('errors', $this->form_validation->error_array());
-            $this->session->set_flashdata('old_data', $this->input->post());
-            redirect('cargo/editar/'.$id_cargo);
-          }
-    } else {
-=======
   public function edit($id_cargo)
   {
     if ($this->input->post()){
@@ -187,7 +161,6 @@ class Cargo extends CI_Controller
       
     }
     else{
->>>>>>> 106eddb22272eb97149544a0ad74427d61778719
       $data['cargo'] = $this->cargo->getById($id_cargo)[0];
       $data['setores']       = $this->setor->get();
       $data['title']         = 'Editar Cargo';      
