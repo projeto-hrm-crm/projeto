73d808426dca70e22ff486fe4933004ac6c87882
pigozzi <rafaelpigozzi@gmail.com>
2018-06-06 19:43:39 -0300

    Ok

--- application/models/Log_model.php
+++ application/models/Log_model.php
@@ -3,7 +3,7 @@
 
     class Log_model extends CI_Model
     {
-        
+
         public function __construct(){
             parent::__construct();
         }
@@ -20,11 +20,11 @@
 
         public function insertLog($tabela, $id, $mensagem, $nome_item)
         {
-            $nome = $this->usuario->getUserNameById($this->session->userdata('user_login'));
+            $nome = $this->usuario->getUserNameById($this->session->userdata('user_login'))->nome;
             $dados['id_usuario'] = $this->session->userdata('user_login');
             $dados['tipo'] = 'insert';
             $dados['acao'] = 'Inserir';
-            $dados['data'] = date('Y-m-d H:i:s');            
+            $dados['data'] = date('Y-m-d H:i:s');
             $dados['tabela'] = $tabela;
             $dados['item_editado'] = $id;
             $dados['descricao'] = $nome . ' ' . $mensagem . ' ' . $nome_item;
@@ -34,11 +34,11 @@
 
         public function updateLog($tabela, $id, $mensagem, $nome_item)
         {
-            $nome = $this->usuario->getUserNameById($this->session->userdata('user_login'));
+            $nome = $this->usuario->getUserNameById($this->session->userdata('user_login'))->nome;
             $dados['id_usuario'] = $this->session->userdata('user_login');
             $dados['tipo'] = 'update';
             $dados['acao'] = 'Atualizar';
-            $dados['data'] = date('Y-m-d H:i:s');            
+            $dados['data'] = date('Y-m-d H:i:s');
             $dados['tabela'] = $tabela;
             $dados['item_editado'] = $id;
             $dados['descricao'] = $nome . ' ' . $mensagem . ' ' . $nome_item;
@@ -48,11 +48,11 @@
 
         public function deleteLog($tabela, $id, $mensagem, $nome_item)
         {
-            $nome = $this->usuario->getUserNameById($this->session->userdata('user_login'));
+            $nome = $this->usuario->getUserNameById($this->session->userdata('user_login'))->nome;
             $dados['id_usuario'] = $this->session->userdata('user_login');
             $dados['tipo'] = 'delete';
             $dados['acao'] = 'Deletar';
-            $dados['data'] = date('Y-m-d H:i:s');            
+            $dados['data'] = date('Y-m-d H:i:s');
             $dados['tabela'] = $tabela;
             $dados['item_editado'] = $id;
             $dados['descricao'] = $nome . ' ' . $mensagem . ' ' . $nome_item;
