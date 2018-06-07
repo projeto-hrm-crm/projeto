1ed5e14b454092bcbef0eec2706920a3c002abfb
baguy <mayradbueno@gmail.com>
2018-06-06 23:50:06 -0300

    02

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,4 @@
-<!-- client CHART -->
+<!-- admin CHART -->
 <div class="col-lg-12">
       <div class="card">
           <div class="card-body"><iframe class="chartjs-hidden-iframe" tabindex="-1" style="display: block; overflow: hidden; border: 0px; margin: 0px; top: 0px; left: 0px; bottom: 0px; right: 0px; height: 100%; width: 100%; position: absolute; pointer-events: none; z-index: -1;"></iframe>
