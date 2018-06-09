f35dcb37f0fd11b283c79ff21163af62bd11f49c
Rodrigo Alves <rodrigodeveloper1@gmail.com>
2018-06-09 17:07:47 -0300

    ajuste informações modal SAC

--- application/views/sac/index.php
+++ application/views/sac/index.php
@@ -83,7 +83,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Excluir produto</h5>
+                <h5 class="modal-title">Excluir</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
