34fdd0ccdcef2d067a41a38f26512ae91b821649
Tiago Lima Villalobos <tiagolimavillalobos@gmail.com>
2018-06-15 16:20:23 -0300

    adequacao padrao SAC

--- application/models/Sac_model.php
+++ application/models/Sac_model.php
@@ -1,100 +1,120 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sac_model extends CI_Model {
-  /**
-  * @author: Rodrigo Alves
-  * Este método inserção de dados
-  *
-  */
-  public function __construct(){
-      parent::__construct();
-  }
-  public function insert($data) {
-
-     try {
-
-      $this->db->insert('sac', $data);
-      $id_sac = $this->db->insert_id();
-
-      if($id_sac)
-      {
-        $this->relatorio->setLog('insert', 'Inserir', 'SAC', $id_sac, 'Inseriu o SAC', $id_sac);
-      }
-      return $id_sac;
-
-    } catch (\Exception $e) {}
-
-  }
-
-  /**
-  * @author: Rodrigo Alves
-  * listar todas as ordens
-  *
-  */
-  public function get() {
-    $query = $this->db->select('*')->from('sac');
-    return $query->get()->result();
-  }
-
-  /**
-  * @author: Rodrigo Alves
-  * listar todas as ordens que o cliente abriu
-  *
-  */
-  public function getClient($id) {
-    $query = $this->db->select('*')->from('sac')->where('id_cliente', $id);
-    return $query->get()->result();
-  }
-
-  /**
-  * @author: Rodrigo Alves
-  * Este método atualiza as informações da ordem do sac referenciado pelo id
-  *
-  */
-  public function update($data, $id) {
-    $this->db->where('id_sac', $id);
-    $id_sac = $this->db->update('sac', $data);
-
-    if($id_sac)
+class Sac_model extends PR_Model 
+{
+    /**
+    * @author: Rodrigo Alves
+    * Este método inserção de dados
+    */
+    public function insert($sac) 
     {
-      $this->relatorio->setLog('update', 'Atualizar', 'SAC', $id_sac, 'Atualizou o SAC', $id);
+        $this->db->insert('sac', $sac);
+
+        $this->setLog($sac['titulo']);
+    
     }
-    return $id_sac;
-  }
 
-  /**
-  * @author: Rodrigo Alves
-  * Alterar estatus sac a
-  *
-  */
-  public function changeStatus($status, $id_sac) {
-    $this->db->set('encerrado', $status);
-    $this->db->where('id_sac', $id_sac);
-    $this->db->update('sac');
-  }
+    /**
+    * @author: Rodrigo Alves
+    * listar todas as ordens
+    */
+    public function get()
+    {   
+        return $this->db
+        ->select('sac.*, pessoa.nome')
+        ->join('cliente', 'sac.id_cliente = cliente.id_cliente')
+        ->join('pessoa', 'cliente.id_pessoa = pessoa.id_pessoa')
+        ->get('sac')
+        ->result();
+    }
 
-  /**
-  * @author: Rodrigo Alves
-  * Apagar uma ordem sac do banco
-  *
-  */
-  public function remove($id) {
+    /**
+    * @author: Tiago Villalobos
+    * Retorna um sac pelo id do mesmo
+    *
+    * @param: $id_sac integer
+    */
+    public function getById($id_sac)
+    {
+        return $this->db
+        ->where('sac.id_sac', $id_sac)
+        ->get('sac')
+        ->result();
+    }
 
+    /**
+    * @author: Rodrigo Alves
+    * listar todas as ordens que o cliente abriu
+    * 
+    * @param: $id_cliente integer
+    */
+    public function getClient($id_cliente) 
+    {
+        return $this->db
+        ->where('id_cliente', $id_cliente)
+        ->get('sac')
+        ->result();  
+    }
 
-    try {
+    /**
+    * @author: Rodrigo Alves
+    * Este método atualiza as informações da ordem do sac referenciado pelo id
+    * 
+    * @param: $sac mixed
+    */
+    public function update($sac) 
+    {
+    
+        $this->db
+        ->set('sac.id_produto', $sac['id_produto'])
+        ->set('sac.id_cliente', $sac['id_cliente'])
+        ->set('sac.titulo', $sac['titulo'])
+        ->set('sac.descricao', $sac['descricao'])
+        ->set('sac.abertura', $sac['abertura'])
+        ->set('sac.fechamento', $sac['fechamento'])
+        ->set('sac.encerrado', $sac['encerrado'])
+        ->where('sac.id_sac', $sac['id_sac'])
+        ->update('sac');
 
-      $this->db->where('id_sac', $id);
-      $id_sac = $this->db->delete('sac');
+        $this->setLog($sac['titulo'], $sac['id_sac']);
 
-      if($id_sac)
-      {
-        $this->relatorio->setLog('delete', 'Deletar', 'SAC', $id_sac, 'Deletou o SAC', $id);
-      }
-      return $id_sac;
+    }
 
-    } catch (\Exception $e) {}
+    /**
+    * @author: Rodrigo Alves
+    * Alterar estatus de um Sac
+    * 
+    * @param: $status boolean
+    * @param: $id_sac integer
+    */
+    public function changeStatus($status, $id_sac) 
+    {
+        $sac = $this->db->where('sac.id_sac', $id_sac)->get('sac')->row();
+        
+        $this->db
+        ->set('encerrado', $status)
+        ->where('id_sac', $id_sac)
+        ->update('sac');
 
-  }
+        $this->setLog($sac->titulo, $id_sac);
+    }
+
+    /**
+    * @author: Rodrigo Alves
+    * Apagar uma ordem sac do banco
+    *
+    * @param: $id_sac integer
+    */
+    public function remove($id_sac) 
+    {
+        $sac = $this->db->where('sac.id_sac', $id_sac)->get('sac')->row();
+
+        $this->db
+        ->where('id_sac', $id_sac)
+        ->delete('sac');
+
+        $this->setLog($sac->titulo, $id_sac);
+    }
 
 }
