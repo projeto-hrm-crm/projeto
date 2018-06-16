572621712b75b67d60e6818abb5aab6e75f0bf02
pigozzi <rafaelpigozzi@gmail.com>
2018-06-15 21:05:14 -0300

    Cargo - OK

--- application/views/cargo/editar.php
+++ application/views/cargo/editar.php
@@ -1,79 +1,78 @@
-<!-- <div class="row justify-content-center">
-  <div class="col-8">
-  <?php if ($this->session->flashdata('success')): ?>
-      <div class="alert alert-success mt-4">
-        <?php echo $this->session->flashdata('success'); ?>
-      </div>
-    <?php endif; ?>
-  </div>
-</div> -->
-<div class="animated fadeIn">
-  <div class="row justify-content-center align-items-center">
+<div class="row justify-content-center align-items-center">
     <div class="col-lg-8">
-      <div class="card">
-        <div class="card-header">
-          <strong class="card-title">Editar Cargo</strong>
-        </div>
-        <form id="form_cargo" action="<?php echo base_url('cargo/editar/'.$cargo->id_cargo);?>" method="post" novalidate="novalidate">
-      
-              <div class="card-body card-block">
-          <div class="row justify-content-center">
-            <div class="col">
-               <!--NOME--> 
-              <div class="form-group">
-              <label class=" form-control-label">Nome</label>
-                <input type="text" id="nome" name="nome" value="<?php echo isset($old_data['nome']) ? $old_data['nome'] : $cargo->nome;?>" name="nome" class="form-control <?php echo isset($errors['nome']) ? 'is-invalid' : '' ?>" required>
-                 <span class="invalid-feedback">
-	                <?php echo isset($errors['nome']) ? $errors['nome'] : '' ; ?>
-	              </span>
-              </div>
-               <!--DESCRIÇÃO--> 
-              <div class="form-group">
-              <label class=" form-control-label">Descrição</label>
-                <textarea placeholder="Descrição do cargo" name="descricao" class="form-control <?php echo isset($errors['descricao']) ? 'is-invalid' : '' ?>"><?php echo isset($old_data['descricao']) ? $old_data['descricao'] : $cargo->descricao;?></textarea>
-                <span class="invalid-feedback">
-	                <?php echo isset($errors['descricao']) ? $errors['descricao'] : '' ; ?>
-	              </span>
-              </div>
-               <!--SALÁRIO--> 
-              <div class="form-group">
-              <label class=" form-control-label">Salário por hora</label>
-                <input id="salario_cargo" type="text" placeholder="Salário por hora" value="<?php echo isset($old_data['salario']) ? $old_data['salario'] : $cargo->salario;?>" name="salario" class="form-control <?php echo isset($errors['salario']) ? 'is-invalid' : '' ?>" required>
-                <span class="invalid-feedback">
-	                <?php echo isset($errors['salario']) ? $errors['salario'] : '' ; ?>
-	              </span>
-              </div>
-               <!--SETOR--> 
-              <div class="form-group">
-              <label class=" form-control-label">Setor</label>
-              <select name="id_setor" id="id_setor" class="form-control <?php echo isset($errors['id_setor']) ? 'is-invalid' : '' ?>">
-                  <option value="">Selecionar</option>
-                    <?php foreach ($setores as $setor): ?>
-                    <option value="<?php echo $setor->id_setor ?>" <?php echo isset($old_data['id_setor']) && ($setor->id_setor == $old_data['id_setor']) || $cargo->id_setor == $setor->id_setor ? 'selected' : '' ?>>
-		                                		<?php echo $setor->nome ?>
-		                                	</option>
-                  <?php endforeach; ?>
-                </select>
-                <span class="invalid-feedback">
-	                             		<?php echo isset($errors['id_setor']) ? $errors['id_setor'] : '' ; ?>
-	                             	</span>
-              </div>
+        <div class="card">
+            <div class="card-header">
+                <strong class="card-title">Editar Produto</strong>
             </div>
-          </div>
-        </div>
-        <div class="card-footer text-right">
-         <!--ACTIONS-->
-        <a href="<?=site_url('cargo')?>" class="btn btn-danger btn-sm">
-                <i class="fa fa-times"></i> Cancelar
-              </a>
-        
-          <button type="submit" class="btn btn-primary btn-sm">
-            <i class="fa fa-dot-circle-o"></i> Salvar
-          </button>
-          
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+            <form id="form_cargo" action="<?php echo base_url('cargo/editar/'.$cargo->id_cargo);?>" method="post" novalidate="novalidate">
+                <div class="card-body">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="form-group col-md-12">
+                                <label for="nome" class="control-label mb-1">Nome do Produto</label>
+                                <input name="nome"  id="nome" value="<?php echo isset($old_data['nome']) ? $old_data['nome'] : $cargo->nome;?>" type="text" class="form-control <?php echo isset($errors['nome']) ? 'is-invalid' : '' ?>" required>
+                                <span class="invalid-feedback">Nome inválido, digite somente letras.</span>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label>Descrição</label>
+                                <textarea placeholder="Descrição do cargo" name="descricao" id="descricao" class="form-control <?php echo isset($errors['descricao']) ? 'is-invalid' : '' ?>" required><?php echo isset($old_data['descricao']) ? $old_data['descricao'] : $cargo->descricao;?></textarea>
+                                <span class="invalid-feedback">Descrição inválida.</span>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="salario" class="control-label mb-1">Salário por hora</label>
+                                <input name="salario" id="salario_cargo" data-thousands="." data-decimal="," placeholder="Salário por hora" value="<?php echo isset($old_data['salario']) ? $old_data['salario'] : $cargo->salario;?>" type="text" class="form-control <?php echo isset($errors['salario']) ? 'is-invalid' : '' ?>" required>
+                                <span class="invalid-feedback">Salário inválido, digite somente números.</span>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <label for="id_setor" class="control-label mb-1">Setor</label>
+                                <select id="id_setor" name="id_setor" class="form-control <?php echo isset($errors['id_setor']) ? 'is-invalid' : '' ?>" required>
+	                                <option value="">Selecione</option>
+	                                <?php foreach ($setores as $setor): ?>
+	                                    <option value="<?php echo $setor->id_setor;?>"
+                                        <?php echo (isset($old_data['id_setor']) && ($setor->id_setor == $old_data['id_setor'])) || !isset($errors['id_setor']) && ($cargo->id_setor == $setor->id_setor) ? 'selected' : '' ?>>
+	                               	    <?php echo $setor->nome;?>
+	                                    </option>
+                                    <?php endforeach; ?>
+	                            </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-right">
+                    <a href="<?php echo base_url('cargo');?>" class="btn btn-danger btn-sm">
+                        <i class="fa fa-times"></i>
+                        Cancelar
+                    </a>
+                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modalAtualizar">
+                        <span class="fa fa-check"></span>
+                        Editar
+                    </button>
+                </div>
+                <div class="modal fade" id="modalAtualizar" role="dialog" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">Editar cargo</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                Deseja realmente editar esse cargo?
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secundary" data-dismiss="modal">
+                                    Cancelar
+                                </button>
+                                <button type="submit" class="btn btn-primary">
+                                    Confirmar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+            </form>
+        </div>
+    </div>
+</div>
