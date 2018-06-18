32addef0f2cc4d893e90945854335ed04625a91a
matheusladislau <matheusladislauribeiro@gmail.com>
2018-06-17 23:41:20 -0300

    criado método find sac por id

--- application/models/Sac_model.php
+++ application/models/Sac_model.php
@@ -1,18 +1,38 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sac_model extends PR_Model 
+class Sac_model extends PR_Model
 {
+  /**
+  * @author: Matheus Ladislau
+  * Retorna registro de sac cadastrados no banco com id_sac referente
+  *
+  * @param integer id_sac refere-se ao id do registro de sac a ser consultado
+  * @return array: registro de sac cadastrado com id_sac informado
+  */
+  public function find($id)
+  {
+    try {
+      $data=$this->db->select('*')->from('sac')->join('produto', 'sac.id_produto=produto.id_produto')->where('sac.id_sac',$id)->get();
+      if($data){
+        return $data->result();
+      }else{
+        // echo "não existe";
+        return 1;
+      }
+      }catch (\Exception $e){}
+      }
+
     /**
     * @author: Rodrigo Alves
     * Este método inserção de dados
     */
-    public function insert($sac) 
+    public function insert($sac)
     {
         $this->db->insert('sac', $sac);
 
         $this->setLog($sac['titulo']);
-    
+
     }
 
     /**
@@ -20,7 +40,7 @@ class Sac_model extends PR_Model
     * listar todas as ordens
     */
     public function get()
-    {   
+    {
         return $this->db
         ->select('sac.*, pessoa.nome')
         ->join('cliente', 'sac.id_cliente = cliente.id_cliente')
@@ -46,26 +66,26 @@ class Sac_model extends PR_Model
     /**
     * @author: Rodrigo Alves
     * listar todas as ordens que o cliente abriu
-    * 
+    *
     * @param: $id_cliente integer
     */
-    public function getClient($id_cliente) 
+    public function getClient($id_cliente)
     {
         return $this->db
         ->where('id_cliente', $id_cliente)
         ->get('sac')
-        ->result();  
+        ->result();
     }
 
     /**
     * @author: Rodrigo Alves
     * Este método atualiza as informações da ordem do sac referenciado pelo id
-    * 
+    *
     * @param: $sac mixed
     */
-    public function update($sac) 
+    public function update($sac)
     {
-    
+
         $this->db
         ->set('sac.id_produto', $sac['id_produto'])
         ->set('sac.titulo', $sac['titulo'])
@@ -83,14 +103,14 @@ class Sac_model extends PR_Model
     /**
     * @author: Rodrigo Alves
     * Alterar estatus de um Sac
-    * 
+    *
     * @param: $status boolean
     * @param: $id_sac integer
     */
-    public function changeStatus($status, $id_sac) 
+    public function changeStatus($status, $id_sac)
     {
         $sac = $this->db->where('sac.id_sac', $id_sac)->get('sac')->row();
-        
+
         $this->db
         ->set('encerrado', $status)
         ->where('id_sac', $id_sac)
@@ -105,7 +125,7 @@ class Sac_model extends PR_Model
     *
     * @param: $id_sac integer
     */
-    public function remove($id_sac) 
+    public function remove($id_sac)
     {
         $sac = $this->db->where('sac.id_sac', $id_sac)->get('sac')->row();
 
@@ -130,7 +150,7 @@ class Sac_model extends PR_Model
                ->join('pessoa as p', 'c.id_pessoa = p.id_pessoa');
       $query = $this->db->get();
 
-      if ($query->num_rows() > 0) 
+      if ($query->num_rows() > 0)
         return $query->result()[0];
       return null;
   }
