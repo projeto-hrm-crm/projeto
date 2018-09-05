8cd2c125c24065da38af8df62a9df33cf2302ac5
carlos <carlos.cadilhe@gmail.com>
2018-09-05 09:45:37 -0300

    Perfil de acesso

--- application/views/landing_page/content.php
+++ application/views/landing_page/content.php
@@ -143,16 +143,23 @@
     <section id="acesso">
         <div class="row acesso-content">
 
-            <div class="col-four acesso-intro">
+            <div class="col-eight acesso-intro">
                 <h1 class="intro-header" data-aos="fade-up">Perfis de Acesso</h1>
 
-                <p data-aos="fade-up">O Perfil de Acesso define quais funcionalidades estarão habilitadas no acesso ao sistema de um grupo de usuários. O administrador pode conceder acessos diferentes para cada perfil de usuário.
+                <p data-aos="fade-up">O Perfil de Acesso define quais funcionalidades estarão habilitadas no acesso ao sistema de um grupo de usuários. 
+                O administrador pode conceder acessos diferentes para cada perfil de usuário. Os grupos de usuários disponíveis sao:
+                <ul>
+                <li>Administrador</li>
+                <li>Cliente</li>
+                <li>Funcionário</li>
+                </ul>
                 </p>
             </div>
 
-            <div class="col-eight acesso-table">
+            <div class="col-four acesso-table">
                 <div class="row">
 
+<!--
                     <div class="col-six plan-wrap">
                         <div class="plan-block" data-aos="fade-up"> 
 
@@ -167,16 +174,17 @@
                      
                         </div>
                     </div> <!-- end plan-wrap -->
+-->
 
-                    <div class="col-six plan-wrap">
+                    <div class="col-twelve plan-wrap">
                         <div class="plan-block primary" data-aos="fade-up">
 
                             <div class="plan-top-part">
-                                <h3 class="plan-block-title">Administrador</h3>
+                                <h3 class="plan-block-title">Área restrita</h3>
                             </div>
 
                             <div class="plan-bottom-part">
-                                <p class="plan-block-features">Acesso total às funções dos sistema</p>                                
+                                <p class="plan-block-features">Demo:<br>Login:<i>admin@admin.com</i><br>Senha:<i>admin</i></p>                                
                                 <a class="button button-primary large" href="<?php echo base_url();?>login" target="_blank">Acessar</a>
                             </div>
 
