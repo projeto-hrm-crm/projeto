337ab8803592667f116af33bbbe229072d2357d5
unknown <rafaelpigozzi@gmail.com>
2018-09-05 15:22:50 -0300

    Posição da mensagem de erro do processo_seletivo

--- application/views/processo_seletivo/editar.php
+++ application/views/processo_seletivo/editar.php
@@ -138,13 +138,15 @@
                 <div class="form-group col-12">
                     <label class="form-control-label">Nome</label>
                     <input type="text" name="nome_etapa[]" placeholder="Nome da etapa" class="form-control" required>
+                </div>
+                <div class="form-group col-12">
                     <label class="form-control-label mt-2">Descrição</label>
                     <textarea auto-resize placeholder="Descrição da etapa"  name="descricao_etapa[]" class="form-control" required></textarea>
-                    <a name="button" class="btn btn-danger btn-sm remDiv mt-2 text-white">
-                        <span class="fa fa-times"></span>
-                        Excluir
-                    </a>
                 </div>
+                <a name="button" class="btn btn-danger btn-sm remDiv mt-2 text-white">
+                    <span class="fa fa-times"></span>
+                    Excluir
+                </a>
             </div>
         </div>
     </div>
