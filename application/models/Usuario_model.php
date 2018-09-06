c99455572dc24061d572abbb84075b934855616e
matheusladislau <matheusladislauribeiro@gmail.com>
2018-09-05 23:40:27 -0300

    colocando adcionando criptografia a senha sha256

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -40,11 +40,13 @@ class Usuario_model extends CI_Model
     */
    public function update($data)
    {
-		$this->db->where('usuario.id_usuario', $data['id_usuario']);
-		$this->db->set('usuario.login', $data['login']);
-		$this->db->update('usuario', $data);
-
-
+       $this->db
+        ->set('usuario.login', $data['login'])
+        ->set('usuario.senha', hash('sha256', $data['senha']))
+        ->set('usuario.id_grupo_acesso', $data['id_grupo_acesso'])
+        ->set('usuario.id_pessoa', $data['id_pessoa'])
+        ->where('usuario.id_usuario', $data['id_usuario']);
+        ->update('usuario');
   }
 
    /**
@@ -102,7 +104,7 @@ class Usuario_model extends CI_Model
         if (!is_null($data)) {
             $email = $data['email'];
             $senha = hash('sha256', $data['senha']);
-            
+
             $this->db->select('*')
                      ->from('usuario')
                      ->where(
