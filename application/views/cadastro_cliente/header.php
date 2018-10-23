ca160a1fe5e818682332dc803e566d93a6cea85a
Rodrigo Alves <rodrigodeveloper1@gmail.com>
2018-10-22 23:02:59 -0200

    backend tela cadastro de usuario

--- application/views/cadastro_cliente/header.php
+++ application/views/cadastro_cliente/header.php
@@ -2,11 +2,10 @@
 <html>
    <head>
       <meta charset="UTF-8">
-      <title>Cadastro de Cliente</title>     
-      
+      <title>Cadastro de Cliente</title>
       <link rel="stylesheet" href="<?php echo base_url();?>assets/client/css/bootstrap.min.css">
       <link rel="stylesheet" href="<?php echo base_url();?>assets/client/css/bootstrap-grid.min.css">
-      <link rel="stylesheet" href="<?php echo base_url();?>assets/client/css/bootstrap-reboot.min.css">      
+      <link rel="stylesheet" href="<?php echo base_url();?>assets/client/css/bootstrap-reboot.min.css">
+      <link rel="stylesheet" href="<?php echo base_url();?>assets/client/css/style.css">
    </head>
-
    <body>
--- application/views/cadastro_cliente/index.php
+++ application/views/cadastro_cliente/index.php
@@ -1,3 +1,84 @@
+<div class="container">
+   <div class="row justify-content-md-center">
+      <div class="col col-lg-10">
+         <div class="card">
+           <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <a class="navbar-brand" href="#"><b>Lambda</b></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
 
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Suporte</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Quem Somos</a>
+                  </li>
+                   <li class="nav-item">
+                    <a class="nav-link" href="#">Documentação</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+           <div class="card-body">
+               
+              
+              
+               <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                 <li class="nav-item">
+                   <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Dados Usuário</a>
+                 </li>
+                 <li class="nav-item">
+                   <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Modulos</a>
+                 </li>
+                 <li class="nav-item">
+                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Empresa</a>
+                 </li>
+                  <li class="nav-item">
+                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Servidor</a>
+                 </li>
+                  <li class="nav-item">
+                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Informações</a>
+                 </li>
+                  
+                  <li class="nav-item">
+                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Endereço</a>
+                 </li>
+               </ul>
+               <div class="tab-content" id="pills-tabContent">
+                 <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                    asdasdasas dsadsdasdas asdsadsadsad sdasds
+                    <div class="input-field">
+                        <label>Nome Completo</label>
+                        <input class="form-control" type="text" placeholder="" name="">
+                    </div>
+                    <div class="input-field">
+                        <label>E-mail</label>
+                        <input class="form-control" type="text" placeholder="" name="">
+                    </div>                    
+                    <div class="input-field margin20">
+                        <label>Nome da Empresa</label>
+                        <input class="form-control" type="text" placeholder="" name="">
+                    </div>
+                    <button class="btn btn-primary">Próximo Passo</button>
+                  </div>
+                 <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+               aaaaaaaaaaaaaaaaaa   
+                  
+                  
+               </div>
+                 <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                  bbbbbbbbbbbbbb
+                  
+                  
+                  </div>
+               </div>
+              
+              
+            </div>
 
-   
+      </div>     
+   </div>   
+</div>
