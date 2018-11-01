8de55763d8fb5800bbd4606d8456d3254c60803a
Rodrigo Alves <rodrigodeveloper1@gmail.com>
2018-10-31 22:59:26 -0200

    formulario sucesso

--- application/views/cadastro_cliente/index.php
+++ application/views/cadastro_cliente/index.php
@@ -34,13 +34,10 @@
                    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-modulo" role="tab" aria-controls="pills-profile" aria-selected="false">Módulos</a>
                  </li>
                  <li class="nav-item">
-                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-empresa" role="tab" aria-controls="pills-contact" aria-selected="false">Empresa</a>
+                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-empresa" role="tab" aria-controls="pills-contact" aria-selected="false">Regionalização</a>
                  </li>
                   <li class="nav-item">
-                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-servidor" role="tab" aria-controls="pills-contact" aria-selected="false">Servidor</a>
-                 </li>
-                  <li class="nav-item">
-                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-informacao" role="tab" aria-controls="pills-contact" aria-selected="false">Informações</a>
+                   <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-informacao" role="tab" aria-controls="pills-contact" aria-selected="false">Dados da Empresa</a>
                  </li>
                   
                   <li class="nav-item">
@@ -57,13 +54,31 @@
                     <div class="input-field">
                         <label>E-mail</label>
                         <input class="form-control" type="text" placeholder="" name="">
+                    </div>
+                    <div class="row">
+                       <div class="col-xs-12 col-lg-6">
+                          <div class="input-field">
+                              <label>Senha</label>
+                              <input class="form-control" type="password" placeholder="" name="">
+                          </div>
+                       </div>
+                       <div class="col-xs-12 col-lg-6">
+                          <div class="input-field">
+                              <label>Confirme a Senha</label>
+                              <input class="form-control" type="password" placeholder="" name="">
+                          </div>
+                       </div>
                     </div>                    
                     <div class="input-field margin20">
-                        <label>Nome da Empresa</label>
-                        <input class="form-control" type="text" placeholder="" name="">
-                    </div>
+                         <label>Finalidade de Uso</label>
+                         <select name="moeda" class="form-control">
+                             <option>Qual a finalidade de uso?</option>
+                             <option value="1">Uso Comercial</option>
+                             <option value="2">Uso Educacional</option>
+                         </select>
+                     </div>
                     <div class="text-right">
-                     <button class="btn btn-primary">Próximo Passo</button>
+                     <button class="btn btn-primary nav-link " id="pills-home-tab" data-toggle="pill" href="#pills-modulo" role="tab" aria-controls="pills-home" aria-selected="true">Próximo Passo</button>
                     </div>
                   </div>
                  <div class="tab-pane fade" id="pills-modulo" role="tabpanel" aria-labelledby="pills-profile-tab">
@@ -76,7 +91,7 @@
                     </div>
                   </div>
                  <div class="tab-pane fade" id="pills-empresa" role="tabpanel" aria-labelledby="pills-contact-tab">
-                     <p class="text-center">Suba uma Imagem, preenchas os dados regionais e finalidade de uso do sistema.
+                    <p class="text-center">Selecione as informações referente a regionalização de seu negócio.</p>
 
                         <div class="input-field">
                             <label>Moeda</label>
@@ -84,27 +99,150 @@
                                 <option>Selecione uma moeda</option>
                             </select>
                         </div>
-                     
-                       <div class="input-field">
-                            <label for="exampleFormControlFile1">Example file input</label>
-                            <input type="file" class="form-control-file" id="exampleFormControlFile1">
-                        </div>                    
+                    
+                        <div class="input-field">
+                            <label>Idioma</label>
+                            <select name="moeda" class="form-control">
+                                <option>Selecione um idioma</option>
+                            </select>
+                        </div>
+                    
+                        <div class="input-field margin20">
+                            <label>Fuso Horário</label>
+                            <select name="moeda" class="form-control">
+                                <option>Selecione um idioma</option>
+                            </select>
+                        </div>
                          
-                     <div class="text-right">
-                        <button class="btn btn-primary">Próximo Passo</button>
-                    </div>
-                  </div>
-                  <div class="tab-pane fade" id="pills-servidor" role="tabpanel" aria-labelledby="pills-profile-tab">
-                  aaaaaaaaaaaaaaaaaa
+                        <div class="text-right">
+                           <button class="btn btn-primary">Próximo Passo</button>
+                        </div>
                   </div>
                  <div class="tab-pane fade" id="pills-informacao" role="tabpanel" aria-labelledby="pills-contact-tab">
-                     bbbbbbbbbbbbbb
+                    <div class="row">
+                    
+                    
+                     <div class="col-xs-12 col-lg-8">
+                        <div class="input-field">
+                           <label>Nome Fantasia</label>
+                           <input class="form-control" type="text" placeholder="" name="">
+                       </div>
+                    </div>
+                       
+                     <div class="col-xs-12 col-lg-4">
+                        <div class="input-field">
+                           <label>Sigla</label>
+                           <input class="form-control" type="text" placeholder="" name="">
+                        </div>
+                     </div>
+                       
+                     <div class="col-xs-12 col-lg-6">
+                       <div class="input-field">
+                           <label>Razão Social</label>
+                           <input class="form-control" type="text" placeholder="" name="">
+                       </div>
+                     </div>
+                        
+                     <div class="col-xs-12 col-lg-6">
+                       <div class="input-field">
+                           <label>CNPJ</label>
+                           <input class="form-control" type="text" placeholder="" name="">
+                       </div>
+                     </div>
+                        
+                     <div class="col-xs-12 col-lg-4" >
+                       <div class="input-field">
+                           <label>Inscrição Estadual</label>
+                           <input class="form-control" type="text" placeholder="" name="">
+                       </div>
+                     </div>
+                       
+                       <div class="col-xs-12 col-lg-4" >
+                          <div class="input-field">
+                              <label>Classificação</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                       </div>
+                       
+                       <div class="col-xs-12 col-lg-4" >
+                          <div class="input-field">
+                              <label>Número Aproximado de Funcionários</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                       </div>
+                       
+                       <div class="col-xs-12 col-lg-12" >
+                          <div class="input-field">
+                              <label>Domínio Site</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                       </div>
+                       
+                       <div class="col-xs-12 col-lg-12" >
+                           <div class="input-field">
+                               <label for="exampleFormControlFile1">Envie sua logo (Dimensão recomendada 400x400px)</label>
+                               <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                           </div>
+                       </div>
+                       
+                       
+                    </div>
+                    <div class="text-right">
+                        <button class="btn btn-primary">Próximo Passo</button>
+                     </div>
                   </div>
                   <div class="tab-pane fade" id="pills-endereco" role="tabpanel" aria-labelledby="pills-contact-tab">
-                     bbbbbbbbbbbbbb
+                     <div class="row">
+                        
+                        <div class="col-xs-12 col-lg-3">
+                           <div class="input-field">
+                              <label>CEP</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                           </div>
+                        </div>
+                        
+                        <div class="col-xs-12 col-lg-7">
+                           <div class="input-field">
+                              <label>Endereço</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                           </div>
+                        </div>
+                           
+                        <div class="col-xs-12 col-lg-2">
+                           <div class="input-field">
+                              <label>Número</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                        </div>
+                        <div class="col-xs-12 col-lg-6">   
+                           <div class="input-field">
+                              <label>Bairro</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                        </div>
+                        <div class="col-xs-12 col-lg-6">
+                           <div class="input-field">
+                              <label>Cidade</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                        </div>
+                        <div class="col-xs-12 col-lg-6">
+                           <div class="input-field">
+                              <label>Estado</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                        </div>
+                        <div class="col-xs-12 col-lg-6">
+                           <div class="input-field">
+                              <label>País</label>
+                              <input class="form-control" type="text" placeholder="" name="">
+                          </div>
+                        </div>
+                        
+                     </div>
                   </div>
-               </div>
             </div>
+         </div>
       </div>     
    </div>   
 </div>
