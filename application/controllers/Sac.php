ec8a69061e05ccc07ad4a09adcc26592441b0b7c
RAFAEL <rafaelpigozzi@gmail.com>
2018-11-07 20:16:03 -0200

    Alterado controller

--- application/controllers/Sac.php
+++ application/controllers/Sac.php
@@ -1,44 +1,50 @@
 <?php
-class Sac extends PR_Controller {
-  public function __construct()
-  {
-      parent::__construct('sac');
-  }
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sac extends CI_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $user_id = $this->session->userdata('user_login');
+        $currentUrl = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+        $this->usuario->hasPermission($user_id, $currentUrl);
+    } 
 
     public function index(){
-      //Pega o id de usuario da sessão
-      $user_id = $this->session->userdata('user_login');
+        //Pega o id de usuario da sessão
+        $user_id = $this->session->userdata('user_login');
 
-      //Pega o tipo de usuario e informações de pessoas
-      $typeUser = $this->usuario->getUserAccessGroup($user_id);
-      $data['pessoa'] = $this->usuario->getUserNameById($user_id);
-      // Pegar informações de cliente
-      $id_pessoa = $data['pessoa'][0]->id_pessoa;  
+        //Pega o tipo de usuario e informações de pessoas
+        $typeUser = $this->usuario->getUserAccessGroup($user_id);
+        $data['pessoa'] = $this->usuario->getUserNameById($user_id);
+        // Pegar informações de cliente
+        $id_pessoa = $data['pessoa'][0]->id_pessoa;  
       
-      if($typeUser=="1"){
-         $data['sac'] = $this->sac->get();
-      }else{
-          $cliente = $this->cliente->getIdCliente($id_pessoa);
-          $id_cliente = $cliente[0]->id_cliente;
-          $data['sac'] = $this->sac->getClient($id_cliente);
-      }
+        if($typeUser=="1"){
+            $data['sac'] = $this->sac->get();
+        } else {
+            $cliente = $this->cliente->getIdCliente($id_pessoa);
+            $id_cliente = $cliente[0]->id_cliente;
+            $data['sac'] = $this->sac->getClient($id_cliente);
+        }
        
-      $data['title'] = 'Solicitações SAC';
-      $data['tipo'] = $typeUser;
+        $data['title'] = 'Solicitações SAC';
+        $data['tipo'] = $typeUser;
        
-      $data['assets'] = array(
-        'js' => array(
-          'lib/data-table/datatables.min.js',
-          'lib/data-table/dataTables.bootstrap.min.js',
-          'datatable.js',
-          'confirm.modal.js',
-        ),
-      );
+        $data['assets'] = array(
+            'js' => array(
+                'lib/data-table/datatables.min.js',
+                'lib/data-table/dataTables.bootstrap.min.js',
+                'datatable.js',
+                'confirm.modal.js',
+            ),
+        );
 
-      foreach ($data['sac'] as $key => $sac) {
-         $cliente = $this->cliente->getById($data['sac'][$key]->id_cliente);
-         $data['sac'][$key]->id_cliente = $cliente[0]->nome;
-      }
+        foreach ($data['sac'] as $key => $sac) {
+            $cliente = $this->cliente->getById($data['sac'][$key]->id_cliente);
+            $data['sac'][$key]->id_cliente = $cliente[0]->nome;
+        }
 
       loadTemplate('includes/header', 'sac/index', 'includes/footer', $data);
     }
@@ -88,52 +94,49 @@ class Sac extends PR_Controller {
 
     public function edit($id) {
 
-       $user_id = $this->session->userdata('user_login');
+        $user_id = $this->session->userdata('user_login');
 
-      $typeUser = $this->usuario->getUserAccessGroup($user_id);
-      $data['pessoa'] = $this->usuario->getUserNameById($user_id);
+        $typeUser = $this->usuario->getUserAccessGroup($user_id);
+        $data['pessoa'] = $this->usuario->getUserNameById($user_id);
        
-      $cliente = $this->cliente->getIdCliente($data['pessoa'][0]->id_pessoa);   
+        $cliente = $this->cliente->getIdCliente($data['pessoa'][0]->id_pessoa);   
 
-      $id_pessoa = $data['pessoa'][0]->id_pessoa;
-      $cliente = $this->cliente->getIdCliente($id_pessoa);
+        $id_pessoa = $data['pessoa'][0]->id_pessoa;
+        $cliente = $this->cliente->getIdCliente($id_pessoa);       
        
-       
-       if($typeUser=="1"){
-         $id_cliente = $this->input->post('id_cliente');
-      }else {
-         $id_cliente = $cliente[0]->id_cliente;
-      }
+        if($typeUser=="1"){
+            $id_cliente = $this->input->post('id_cliente');
+        } else {
+            $id_cliente = $cliente[0]->id_cliente;
+        }
       
-       $data = $this->input->post();
+        $data = $this->input->post();
 
-         if($data){
+        if($data){
             if ($this->form_validation->run('sac')) {
+                if($this->input->post('encerrado')){
+                    $fec = date("Y-m-d H:i:s");
+                } else {
+                    $fec = 0;
+                }
 
-               if($this->input->post('encerrado')){
-                  $fec = date("Y-m-d H:i:s");
-               }else {
-                  $fec = 0;
-               }
+                $array = array(
+                    'id_produto' => $this->input->post('id_produto'),
+                    'abertura' => date("Y-m-d H:i:s"),
+                    'fechamento' => $fec,
+                    'encerrado' => $this->input->post('encerrado'),
+                    'titulo' => $this->input->post('titulo'),
+                    'descricao' => $this->input->post('descricao'),
+                    'id_sac'=>$id,
+                );
+                $this->sac->update($array, $id);
+                $this->session->set_flashdata('success', 'SAC atualizado com sucesso!');
 
-               $array = array(
-                 'id_produto' => $this->input->post('id_produto'),
-                 'abertura' => date("Y-m-d H:i:s"),
-                 'fechamento' => $fec,
-                 'encerrado' => $this->input->post('encerrado'),
-                 'titulo' => $this->input->post('titulo'),
-                 'descricao' => $this->input->post('descricao'),
-                 'id_sac'=>$id,
-               );
-               $this->sac->update($array, $id);
-               $this->session->set_flashdata('success', 'SAC atualizado com sucesso!');
-
-               redirect('sac');
-            }else{
-               $this->session->set_flashdata('danger', 'SAC não pode ser atualizado!');
-               redirect('sac');
+                redirect('sac');
+            } else {
+                $this->session->set_flashdata('danger', 'SAC não pode ser atualizado!');
+                redirect('sac');
             }
-
         }
         else {
             $data['title']='Editar Sac';
