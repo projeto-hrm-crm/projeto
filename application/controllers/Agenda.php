30eb6086baa32ffb46cd1157a529d67f13073077
RAFAEL <rafaelpigozzi@gmail.com>
2018-11-07 21:45:08 -0200

    Permissões

--- application/controllers/Agenda.php
+++ application/controllers/Agenda.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Agenda extends CI_Controller
 {
+
+    public function __construct() 
+    {
+        parent::__construct();
+        $user_id = $this->session->userdata('user_login');
+        $currentUrl = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '';
+        $this->usuario->hasPermission($user_id, $currentUrl);
+    }
+
     public function index()
     {
         $dados['title'] = 'Agenda';
